88e2dda62d475a2cd4bccc5505b7fe5ed330814f
gonzalayno <87550531+gonzalayno@users.noreply.github.com>
2025-03-05 10:40:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ app.post('/download', (req, res) => {
 
     const fullDownloadPath = path.join(downloadPath, '%(title)s.%(ext)s');
 
-    // Ejecutar spotdl con el formato y la ruta seleccionados
-    const downloadProcess = spawn('spotdl', ['download', url, '--format', format, '--output', '/home/glayno/Descargas/testing_musica']);
+    // Ejecutar spotdl con el formato y la ruta seleccionadossvcvc
+    const downloadProcess = spawn('spotdl', ['download', url, '--format', format, '--output', '/home/glayno/Descargas/testing_musica','--proxy "socks5://192.252.214.20:15864"']);
 
     downloadProcess.stdout.on('data', (data) => {
         console.log(`Salida: ${data}`);
